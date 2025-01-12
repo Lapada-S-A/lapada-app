361cc1893bf21340bbc1996bf87f0af8d18284a5
marcelakramer <101991467+marcelakramer@users.noreply.github.com>
2025-01-11 22:12:08 -0300

    feat: create auction card

---
+++ components/auctions/AuctionCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <v-card width="250" height="300" class="pa-6 d-flex flex-column">
+      <v-img src="@/assets/images/auction.png" class="mb-3" height="155" />
+      <div class="flex-grow-1">
+        <div class="font-large font-weight-bold mb-1 title-truncate">
+          {{ title }}
+        </div>
+        <div class="font-small font-weight-semibold text-primary mb-2">
+          {{ type }}
+        </div>
+      </div>
+
+      <!-- Time and bid at the bottom -->
+      <div class="d-flex justify-space-between font-small font-weight-bold">
+        <div class="d-flex align-center ga-1">
+          <v-icon color="secondary">mdi-clock-fast</v-icon>
+          {{ remainingTime }}
+        </div>
+        <div class="d-flex align-center ga-1">
+          <v-icon color="secondary">mdi-cash-multiple</v-icon>
+          {{ highestBid }}
+        </div>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  type: string;
+  remainingTime: string;
+  highestBid: string;
+}>();
+</script>
+
+<style scoped>
+:deep(.v-card) {
+  color: rgba(var(--v-theme-font-100));
+}
+
+.title-truncate {
+  line-height: 20px;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: normal;
+  line-clamp: 2;
+  -webkit-line-clamp: 2;
+}
+</style>
--- pages/auctions/index.vue
+++ pages/auctions/index.vue
@@ -1,1 +1,44 @@
-<template><div>auctions ne bbr</div></template>
+<template>
+  <div class="d-flex flex-wrap ga-4">
+    <div v-for="auction in auctions" :key="auction.title">
+      <AuctionCard
+        :title="auction.title"
+        :type="auction.type"
+        :remaining-time="auction.remainingTime"
+        :highest-bid="auction.highestBid"
+        :width="auction.width"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AuctionCard from "~/components/auctions/AuctionCard.vue";
+
+const auctions = [
+  {
+    title: "Pintura de Paisagem do Século XIX",
+    type: "Leilão invertido",
+    remainingTime: "2d3h",
+    highestBid: "280.000",
+  },
+  {
+    title: "Escultura de Mármore",
+    type: "Leilão tradicional",
+    remainingTime: "5h30m",
+    highestBid: "150.000",
+  },
+  {
+    title: "Relógio de Ouro Antigo",
+    type: "Leilão por proximidade",
+    remainingTime: "1d12h",
+    highestBid: "500.000",
+  },
+  {
+    title: "Carro Clássico",
+    type: "Leilão invertido",
+    remainingTime: "3d1h",
+    highestBid: "1.200.000",
+  },
+];
+</script>
