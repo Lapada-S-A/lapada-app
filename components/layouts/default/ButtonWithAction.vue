11a05930269b90aa73252b3493387eddcabb3194
juanfarias <juanfariastk@gmail.com>
2025-01-24 01:08:28 -0300

    feat: button default to dashboard

---
+++ components/layouts/default/ButtonWithAction.vue
@@ -0,0 +1,81 @@
+<template>
+    <v-btn
+      v-if="showButton"
+      :id="buttonId"
+      class="rounded-lg border d-flex flex-column align-center justify-center large-square-btn ma-2"
+      :color="color"
+      stacked
+      :elevation="elevation"
+      :outlined="outlined"
+      :style="{ width: `${width}px`, height: `${height}px` }"
+      @click="handleClick"
+    >
+      <v-icon v-if="icon" :size="iconSize" class="mb-2">{{ icon }}</v-icon>
+      <div  class="text-center" >{{ label }}</div>
+    </v-btn>
+  </template>
+  
+  <script setup lang="ts">
+  const props = defineProps({
+    label: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      default: null,
+    },
+    iconSize: {
+      type: [Number, String],
+      default: 40,
+    },
+    color: {
+      type: String,
+      default: 'primary',
+    },
+    elevation: {
+      type: [Number, String],
+      default: 2,
+    },
+    outlined: {
+      type: Boolean,
+      default: true,
+    },
+    buttonId: {
+      type: String,
+      default: 'icon-button',
+    },
+    showButton: {
+      type: Boolean,
+      default: true,
+    },
+    width: {
+      type: [Number, String],
+      default: 270, 
+    },
+    height: {
+      type: [Number, String],
+      default: 160,
+    },
+    onClick: {
+      type: Function,
+      default: null,
+    },
+  })
+  
+  const handleClick = () => {
+    if (props.onClick) {
+      props.onClick()
+    }
+  }
+  </script>
+  
+  <style scoped>
+  
+  .large-square-btn {
+    text-transform: none;
+    font-size: 16px;
+    padding: 0;
+  }
+  </style>
+  
