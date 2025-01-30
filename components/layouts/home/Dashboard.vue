0f95357a7ec49a8a8fa38f4e4b7fb40f8d52c76d
Juan Farias <juan.farias@assert.ifpb.edu.br>
2025-01-30 16:22:23 -0300

    feat: create folter to home components and change layout.vue folder

---
+++ components/layouts/home/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+      <div class="d-flex flex-wrap justify-center align-start gap-4">
+        <LayoutsDefaultButtonWithAction
+          v-for="action in routeStore.menuRoutes"
+          :key="action.label"
+          :label="action.label"
+          :icon="action.icon"
+          :onClick="() => navigateTo(action.route)"
+        />
+      </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { useRouter } from "vue-router";
+  const router = useRouter();
+  const routeStore  = useRoutesStore();
+
+  const navigateTo = (route:string) => {
+    router.push(route);
+  };
+  </script>
+  
+  <style scoped>
+  .gap-4 {
+    gap: 16px;
+  }
+  </style>
+  
