946825c3f2a51e7e483979a5a21d421ec6b43a4a
marcelakramer <101991467+marcelakramer@users.noreply.github.com>
2025-01-30 01:39:55 -0300

    feat: create common main card

---
+++ components/common/MainCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card class="calc-height pa-6">
+    <slot />
+  </v-card>
+</template>
+
+<style scoped>
+.calc-height {
+  height: calc(100vh - 90px) !important;
+  border-radius: 6px;
+}
+</style>
---
+++ pages/bids/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div><MainCard>lances ne bbr</MainCard></div>
+</template>
+
+<script setup lang="ts">
+import MainCard from "~/components/common/MainCard.vue";
+</script>
