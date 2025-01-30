0638ef280756ea272d9140e222ede5ddd68a9621
marcelakramer <101991467+marcelakramer@users.noreply.github.com>
2025-01-30 00:13:48 -0300

    feat: create no results found message

--- components/auctions/AuctionList.vue
+++ components/auctions/AuctionList.vue
@@ -2,23 +2,36 @@
   <div>
     <AuctionSearch v-model:search-query="searchQuery" />
 
-    <div class="d-flex flex-wrap ga-4">
-      <div v-for="auction in filteredAuctions" :key="auction.title">
-        <AuctionCard
-          :title="auction.title"
-          :type="auction.type"
-          :remaining-time="auction.remainingTime"
-          :highest-bid="auction.highestBid"
-        />
+    <div
+      v-if="filteredAuctions.length === 0"
+      class="d-flex justify-center mt-16"
+    >
+      <div class="no-results-card d-flex flex-column align-center justify-center pa-6 mt-8" max-width="400">
+        <v-icon size="125" color="secondary">mdi-magnify</v-icon>
+        <div class="mt-4 font-subtitle font-weight-semibold text-secondary text-center">Nenhum leilão encontrado</div>
       </div>
     </div>
-    <div class="d-flex justify-center ga-2 mt-16">
-      <v-btn class="btn btn-secondary font-small" height="32" width="85"
-        >Anterior</v-btn
-      >
-      <v-btn class="btn btn-primary font-small" height="32" width="85"
-        >Próxima</v-btn
-      >
+
+    <div v-else>
+      <div class="d-flex flex-wrap ga-4">
+        <div v-for="auction in filteredAuctions" :key="auction.title">
+          <AuctionCard
+            :title="auction.title"
+            :type="auction.type"
+            :remaining-time="auction.remainingTime"
+            :highest-bid="auction.highestBid"
+          />
+        </div>
+      </div>
+
+      <div class="d-flex justify-center ga-2 mt-16">
+        <v-btn class="btn btn-secondary font-small" height="32" width="85">
+          Anterior
+        </v-btn>
+        <v-btn class="btn btn-primary font-small" height="32" width="85">
+          Próxima
+        </v-btn>
+      </div>
     </div>
   </div>
 </template>
@@ -62,3 +75,10 @@ const filteredAuctions = computed(() => {
   );
 });
 </script>
+
+<style scoped>
+.no-results-card {
+  background-color: rgba(var(--v-theme-font-10), 0.7);
+  border-radius: 6px;
+}
+</style>
