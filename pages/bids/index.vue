c2eb4aef603abf38a8617fffa5c831c57b72c807
Marcela Kramer <marcela.kramer@assert.ifpb.edu.br>
2025-01-30 15:13:31 -0300

    feat: create filter options

--- pages/bids/index.vue
+++ pages/bids/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <MainCard>
+      <AuctionFilterOptions />
       <AuctionPanelsList />
     </MainCard>
   </div>
@@ -8,5 +9,6 @@
 
 <script setup lang="ts">
 import MainCard from "~/components/common/MainCard.vue";
+import AuctionFilterOptions from "~/components/bids/AuctionFilterOptions.vue";
 import AuctionPanelsList from "~/components/bids/AuctionPanelsList.vue";
 </script>
