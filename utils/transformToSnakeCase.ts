d987833dee1a64511c31256c36b47a272257987d
juanfarias <juanfariastk@gmail.com>
2025-01-31 09:06:52 -0300

    feat: integration with endpoint FULL

---
+++ utils/transformToSnakeCase.ts
@@ -0,0 +1,17 @@
+const toSnakeCase = (str: string) => {
+    return str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+};
+
+export const transformToSnakeCase = (filters: Record<string, any>) => {
+    return Object.fromEntries(
+      Object.entries(filters)
+        .map(([key, value]) => {
+          if (value && typeof value === "object" && "id" in value) {
+            value = value.id;
+          }
+          return [toSnakeCase(key), value];
+        })
+        .filter(([, value]) => value !== null && value !== undefined)
+    );
+  };
+  
