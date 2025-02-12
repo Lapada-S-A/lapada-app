244833a8cc27d45a7ef1ef7f1a853a0c04da54a7
marcelakramer <101991467+marcelakramer@users.noreply.github.com>
2025-02-12 17:10:07 -0300

    chore: change port

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   devServer: {
-    port: 3001,
+    port: 3000,
   },
   ssr: false,
   build: {
