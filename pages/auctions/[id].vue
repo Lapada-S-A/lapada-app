c8346908eb89f051a1989b094f4b599cb32daa30
juanfarias <juanfariastk@gmail.com>
2025-02-13 01:32:50 -0300

    feat: catch data with endpoint

--- pages/auctions/[id].vue
+++ pages/auctions/[id].vue
@@ -1,14 +1,20 @@
 <template>
   <AuctionDetails
-    :title="auctionData.title"
-    :category="auctionData.category"
-    :date="auctionData.date"
-    :current-bid="auctionData.currentBid"
-    :min-increment="auctionData.minIncrement"
-    :remaining-time="auctionData.remainingTime"
-    :description="auctionData.description"
+    v-if="auction"
+    :title="auction.title"
+    :auction-type="auctionType?.name"
+    :category="auction.categories.map((category) => category.name).join(', ')"
+    :date="auction.created_date ? formatDate(auction.created_date) : ''"
+    :current-bid="formatCurrency(auction.highest_bid || auction.initial_value)"
+    :min-increment="formatCurrency(auction.min_increment)"
+    :remaining-time="
+      auction.created_date && auction.end_date
+        ? calculateRemainingTime(auction.created_date, auction.end_date)
+        : 'N/A'
+    "
+    :description="auction.description"
     :images="auctionData.images"
-    :bids="auctionData.bids"
+    :bids="auctionBids || []"
     :auction-id="auctionId"
     :show-back-button="true"
   />
@@ -16,31 +22,71 @@
 
 <script setup lang="ts">
 import AuctionDetails from "@/components/auctions/details/index.vue";
+import type { Auction } from "~/interfaces/auction";
+import type { Bid } from "~/interfaces/bid";
+import type { Type } from "~/interfaces/type";
 
 const route = useRoute();
 const auctionId = Number(route.params.id);
+const auctionsStore = useAuctionsStore();
+const bidsStore = useBidsStore();
+const typesStore = useTypesStore();
+
+const auctionType = ref<Type>();
+const auction = ref<Auction>();
+const auctionBids = ref<Bid[]>();
+
+onMounted(async () => {
+  auction.value = await auctionsStore.getAuctionById(auctionId);
+  auctionBids.value = await bidsStore.getBidsByAuctionId(auctionId);
+  auctionType.value = typesStore.getTypeById(auction.value?.type_id || 0);
+});
 
 const auctionData = {
-  title: "Automóvel Clássico Ford Mustang 1965",
-  category: "Automóvel, Relíquia",
-  date: "21/11/2024",
-  currentBid: "R$ 500.000,00",
-  minIncrement: "R$ 5.000,00",
-  remainingTime: "3 dias 14 horas 26 minutos 58 segundos",
-  description: `Apresentamos um icônico Ford Mustang 1965, um verdadeiro símbolo da era de ouro dos muscle cars americanos. Este exemplar não é apenas um carro, mas uma verdadeira relíquia histórica que foi completamente restaurada com um nível de precisão impressionante, respeitando rigorosamente cada detalhe do modelo original. O trabalho de restauração foi feito com extrema dedicação, garantindo que este Mustang esteja em um estado impecável, tanto estético quanto mecânico, pronto para encantar os entusiastas de carros clássicos e os apreciadores de desempenho.
-  Equipado com um potente motor V8 289, este Mustang oferece uma experiência de direção robusta e emocionante, que é a essência do que fez o modelo se tornar uma lenda entre os carros de performance. Sua aceleração enérgica e o ronco inconfundível do motor fazem qualquer entusiasta sentir a verdadeira emoção de estar no comando de um carro com tanta história. A suspensão foi cuidadosamente ajustada, proporcionando uma dirigibilidade precisa e confortável, sem perder a essência de um muscle car puro.
-  A pintura original vermelha, restaurada com um processo meticuloso, brilha intensamente sob a luz, realçando as linhas clássicas e elegantes que definem o design atemporal deste carro. O brilho da pintura é acompanhado por um polido impecável, que garante um visual vibrante e cativante, digno de um carro de colecionador. As rodas de liga leve cromadas, de excelente acabamento, complementam o visual icônico do Mustang, conferindo um toque de sofisticação e esportividade.
-  Este Mustang 1965 não é apenas um carro, mas uma verdadeira peça de colecionador, ideal para quem busca um exemplar raro e em perfeitas condições de conservação. Com um histórico de cuidados detalhados e um visual que não passa despercebido, este modelo representa a fusão perfeita entre tradição, desempenho e estilo, fazendo dele uma escolha incomparável para qualquer fã de carros clássicos.`,
   images: [
     "https://cdn.motor1.com/images/mgl/jlw7Ne/s1/bmw-m5-2024-im-test.jpg",
     "https://cdn.motor1.com/images/mgl/jlwrMo/s1/novo-bmw-serie-3-2027---projecao.jpg",
     "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/03/BMW-iX1eDrive20-xLine-e1710447790402.jpeg",
     "https://i.ytimg.com/vi/B3sh4cjPs88/maxresdefault.jpg",
   ],
-  bids: [
-    { user: "Pablo Estrela", date: "29/11/2024", value: "R$ 500.000,00" },
-    { user: "Juan Farias", date: "27/11/2024", value: "R$ 455.000,00" },
-    { user: "Pablo Estrela", date: "26/11/2024", value: "R$ 450.000,00" },
-  ],
+};
+
+const calculateRemainingTime = (startDate: string, endDate: string): string => {
+  const start = new Date(startDate).getTime();
+  const end = new Date(endDate).getTime();
+  const diff = end - start;
+
+  if (diff <= 0) return "Encerrado";
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+};
+
+const formatDate = (dateString: string): string => {
+  if (!dateString) return "";
+
+  const parsedDate = Date.parse(dateString)
+    ? new Date(dateString)
+    : parseCustomDate(dateString);
+
+  if (!parsedDate || isNaN(parsedDate.getTime())) return "Data inválida";
+
+  const day = String(parsedDate.getDate()).padStart(2, "0");
+  const month = String(parsedDate.getMonth() + 1).padStart(2, "0");
+  const year = parsedDate.getFullYear();
+
+  return `${day}/${month}/${year}`;
+};
+
+const parseCustomDate = (dateString: string): Date => {
+  const parts = dateString.split("-");
+  if (parts.length < 3) return new Date(NaN);
+
+  const [day, month, year, hour, minute, second] = parts.map(Number);
+  return new Date(year, month - 1, day, hour || 0, minute || 0, second || 0);
 };
 </script>
