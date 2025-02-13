2fde6b2b489040e0e0e963a34de70a093a4efc4a
juanfarias <juanfariastk@gmail.com>
2025-02-12 21:37:33 -0300

    feat: additing validation to render a correct button and send receive a auction id prop

--- components/auctions/details/MainDetails.vue
+++ components/auctions/details/MainDetails.vue
@@ -1,17 +1,32 @@
 <template>
   <div>
-    <div class="text-primary font-weight-bold font-title mb-1">{{ auctionType }}</div>
+    <div class="text-primary font-weight-bold font-title mb-1">
+      {{ auctionType }}
+    </div>
     <div class="font-weight-bold text-font-100 font-bigtitle">{{ title }}</div>
     <div class="text-font-60 font-small mb-1">{{ category }}</div>
     <div class="text-font-100">{{ date }}</div>
     <v-row class="mt-1">
       <v-col cols="12">
         <div class="font-weight-semibold text-primary">Lance atual</div>
-        <div class="text-bid text-font-100 font-weight-bold ml-n1 mt-n3">{{ currentBid }}</div>
-        <div class="text-font-60 mt-n1">Incremento mín.: {{ minIncrement }}</div>
+        <div class="text-bid text-font-100 font-weight-bold ml-n1 mt-n3">
+          {{ currentBid }}
+        </div>
+        <div class="text-font-60 mt-n1">
+          Incremento mín.: {{ minIncrement }}
+        </div>
       </v-col>
       <v-col cols="12" class="d-flex justify-center mt-4 mb-5">
-        <CreateBidDialog />
+        <CreateBidDialog
+          v-if="isUserType1"
+          :auction-id="auctionId"
+          :buyer-id="currentUser?.id ?? 0"
+        />
+        <DualButton
+          v-else
+          :left-button-text="leftButtonText"
+          :right-button-text="rightButtonText"
+        />
       </v-col>
     </v-row>
   </div>
@@ -19,6 +34,8 @@
 
 <script setup lang="ts">
 import CreateBidDialog from "@/components/auctions/details/CreateBidDialog.vue";
+import { useUserStore } from "@/stores/user";
+import DualButton from "~/components/common/DualButton.vue";
 
 defineProps({
   title: { type: String, required: true },
@@ -27,6 +44,24 @@ defineProps({
   date: { type: String, required: true },
   currentBid: { type: String, required: true },
   minIncrement: { type: String, required: true },
+  auctionId: { type: Number, required: true },
+});
+
+const userStore = useUserStore();
+const currentUser = userStore.currentUser;
+
+const isUserType1 = computed(() => currentUser?.type_user === 1);
+
+const leftButtonText = computed(() => {
+  if (currentUser?.type_user === 3) return "Aprovar";
+  if (currentUser?.type_user === 2) return "Cancelar";
+  return "Cancelar";
+});
+
+const rightButtonText = computed(() => {
+  if (currentUser?.type_user === 3) return "Rejeitar";
+  if (currentUser?.type_user === 2) return "Encerrar";
+  return "Encerrar";
 });
 </script>
 
