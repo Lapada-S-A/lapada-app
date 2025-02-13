4a074c9a11f4a1ba25967b477d809d47863bf283
juanfarias <juanfariastk@gmail.com>
2025-02-13 01:32:30 -0300

    feat: additing endpoint usage of change status and actions to dual button

--- components/auctions/details/MainDetails.vue
+++ components/auctions/details/MainDetails.vue
@@ -26,18 +26,29 @@
           v-else
           :left-button-text="leftButtonText"
           :right-button-text="rightButtonText"
+          @left-click="confirmAction(leftClickActions, leftButtonText)"
+          @right-click="confirmAction(rightClickActions, rightButtonText)"
         />
       </v-col>
     </v-row>
+
+    <ConfirmationDialog
+      v-model="showConfirmDialog"
+      :text="'Tem certeza que deseja '"
+      :highlight="actionText"
+      @confirm="executeConfirmedAction"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import CreateBidDialog from "@/components/auctions/details/CreateBidDialog.vue";
 import { useUserStore } from "@/stores/user";
+import { ref } from "vue";
+import ConfirmationDialog from "~/components/common/ConfirmationDialog.vue";
 import DualButton from "~/components/common/DualButton.vue";
 
-defineProps({
+const props = defineProps({
   title: { type: String, required: true },
   auctionType: { type: String, default: "Leilão Comum" },
   category: { type: String, required: true },
@@ -48,6 +59,7 @@ defineProps({
 });
 
 const userStore = useUserStore();
+const auctionStore = useAuctionsStore();
 const currentUser = userStore.currentUser;
 
 const isUserType1 = computed(() => currentUser?.type_user === 1);
@@ -63,6 +75,43 @@ const rightButtonText = computed(() => {
   if (currentUser?.type_user === 2) return "Encerrar";
   return "Encerrar";
 });
+
+const showConfirmDialog = ref(false);
+const actionToExecute = ref<(() => Promise<void>) | null>(null);
+const actionText = ref("");
+
+const confirmAction = (action: () => Promise<void>, text: string) => {
+  actionToExecute.value = action;
+  actionText.value = text;
+  showConfirmDialog.value = true;
+};
+
+const executeConfirmedAction = async () => {
+  if (actionToExecute.value) {
+    await actionToExecute.value();
+  }
+};
+
+const leftClickActions = async () => {
+  if (!currentUser || !props.auctionId) return;
+
+  if (currentUser.type_user === 3) {
+    //ainda sem comportamento definido
+    await auctionStore.changeStatusOfAuction(props.auctionId, "open");
+  } else if (currentUser.type_user === 2) {
+    await auctionStore.changeStatusOfAuction(props.auctionId, "cancel");
+  }
+};
+
+const rightClickActions = async () => {
+  if (!currentUser || !props.auctionId) return;
+
+  if (currentUser.type_user === 3) {
+    await auctionStore.changeStatusOfAuction(props.auctionId, "reject");
+  } else if (currentUser.type_user === 2) {
+    await auctionStore.changeStatusOfAuction(props.auctionId, "finish");
+  }
+};
 </script>
 
 <style scoped>
