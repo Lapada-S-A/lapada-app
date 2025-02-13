c26194d4a1f0682f009b6c052258a06d94fd6b70
juanfarias <juanfariastk@gmail.com>
2025-02-13 01:29:12 -0300

    refactor: additing type bid to last bids

--- components/auctions/details/LastBids.vue
+++ components/auctions/details/LastBids.vue
@@ -2,20 +2,20 @@
   <div class="mt-5">
     <div class="font-weight-semibold text-primary">
       Últimos lances
-      <LastBidsDialog :bids="bids"/>
+      <LastBidsDialog :bids="bids" />
     </div>
-    <LastBidsTable :bids="bids.slice(0,3)"/>
+    <LastBidsTable :bids="bids.slice(0, 3)" />
   </div>
 </template>
 <script setup lang="ts">
 import LastBidsDialog from "@/components/auctions/details/LastBidsDialog.vue";
 import LastBidsTable from "@/components/auctions/details/LastBidsTable.vue";
+import type { Bid } from "~/interfaces/bid";
 
 defineProps({
   bids: {
-    type: Array as () => { user: string; date: string; value: string }[],
+    type: Array as () => Bid[],
     required: true,
   },
 });
 </script>
-
