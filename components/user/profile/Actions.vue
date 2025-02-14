f304ae37073fdf3679e190b9b383dcc3e5ca27bf
Marcela Kramer <marcelabarretokramer10@gmail.com>
2025-02-14 09:04:02 -0300

    feat: adjust in profile page

--- components/user/profile/Actions.vue
+++ components/user/profile/Actions.vue
@@ -7,20 +7,30 @@
       :height="button.height"
       :width="button.width"
       :prepend-icon="button.icon"
+      :disabled="button.disabled"
       @click="button.click"
     >
       {{ button.text }}
     </v-btn>
     <ChangePasswordDialog ref="changePasswordDialog" />
+    <CommonConfirmationDialog
+      v-model="confirmDeleteAccount"
+      text="Você tem certeza que deseja"
+      highlight="apagar sua conta"
+      @confirm="deleteAccount"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { User } from "~/interfaces/user";
 import ChangePasswordDialog from "./ChangePasswordDialog.vue";
+import { UserTypes } from "~/stores/enum";
 
 const userStore = useUserStore();
 const changePasswordDialog = ref();
+const confirmDeleteAccount = ref<boolean>(false);
+const router = useRouter();
 
 const props = defineProps<{ user: User | null }>();
 
@@ -31,11 +41,9 @@ const buttons = ref([
     width: "200",
     icon: "mdi-badge-account",
     text: "Tornar-se vendedor",
+    disabled: userStore.currentUser?.type_user === UserTypes.Seller,
     click: () => {
-      if (props.user?.id) {
-        useSnackbarStore().showSnackbar("success", "Você agora é um vendedor!");
-        userStore.promoterToSeller(props.user.id);
-      }
+      promoterToSeller();
     },
   },
   {
@@ -44,6 +52,7 @@ const buttons = ref([
     width: "200",
     icon: "mdi-pencil-outline",
     text: "Alterar senha",
+    disabled: false,
     click: () => {
       changePasswordDialog.value.showDialog = true;
     },
@@ -54,11 +63,23 @@ const buttons = ref([
     width: "200",
     icon: "mdi-delete-outline",
     text: "Apagar conta",
-    click: () => {
-      if (props.user?.id) {
-        userStore.deleteAccount(props.user.id);
-      }
-    },
+    disabled: false,
+    click: () => (confirmDeleteAccount.value = true),
   },
 ]);
+
+function deleteAccount() {
+  if (props.user?.id) {
+    userStore.deleteAccount(props.user!.id!);
+  }
+}
+
+function promoterToSeller() {
+  if (props.user?.id) {
+    userStore.promoterToSeller(props.user.id);
+    userStore.getUserById(props.user.id);
+    useSnackbarStore().showSnackbar("success", "Você agora é um vendedor!");
+    router.push("/");
+  }
+}
 </script>
--- components/user/profile/ChangePasswordDialog.vue
+++ components/user/profile/ChangePasswordDialog.vue
@@ -1,57 +1,173 @@
 <template>
-  <v-dialog v-model="showDialog" max-width="500">
-    <v-card>
-      <v-card-title>Alterar Senha</v-card-title>
-      <v-card-text>
-        <v-form @submit.prevent="changePassword">
-          <v-text-field
-            v-model="currentPassword"
-            label="Senha Atual"
-            type="password"
-            required
-          />
-          <v-text-field
-            v-model="newPassword"
-            label="Nova Senha"
-            type="password"
-            required
-          />
-          <v-text-field
-            v-model="confirmPassword"
-            label="Confirmar Nova Senha"
-            type="password"
-            required
-          />
-          <v-btn type="submit" color="primary">Alterar Senha</v-btn>
-        </v-form>
+  <v-dialog v-model="showDialog" opacity="0.4" max-width="500" persistent>
+    <v-card class="rounded-lg pa-4">
+      <div>
+        <v-card-title class="d-flex justify-space-between align-center">
+          <div class="font-subtitle font-weight-bold">Alterar senha</div>
+          <v-btn
+            id="close-filter-button"
+            class="rounded-xl"
+            variant="plain"
+            :ripple="false"
+            size="22"
+            @click="cancel"
+          >
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+      </div>
+
+      <v-card-text class="px-4 py-4">
+        <div class="font-weight-medium mb-2">Senha atual</div>
+        <v-text-field
+          v-model="currentPassword"
+          placeholder="Senha atual"
+          :type="currentPasswordVisible ? 'text' : 'password'"
+          required
+        >
+          <template #append-inner>
+            <v-icon
+              v-if="currentPasswordVisible"
+              class="cursor-pointer"
+              @click="toggleCurrentPasswordVisibility"
+            >
+              mdi-eye-off
+            </v-icon>
+            <v-icon
+              v-else
+              class="cursor-pointer"
+              @click="toggleCurrentPasswordVisibility"
+            >
+              mdi-eye
+            </v-icon>
+          </template>
+        </v-text-field>
+
+        <div class="font-weight-medium mb-2">Nova senha</div>
+        <v-text-field
+          v-model="newPassword"
+          placeholder="Nova senha"
+          :type="newPasswordVisible ? 'text' : 'password'"
+          required
+        >
+          <template #append-inner>
+            <v-icon
+              v-if="newPasswordVisible"
+              class="cursor-pointer"
+              @click="toggleNewPasswordVisibility"
+            >
+              mdi-eye-off
+            </v-icon>
+            <v-icon
+              v-else
+              class="cursor-pointer"
+              @click="toggleNewPasswordVisibility"
+            >
+              mdi-eye
+            </v-icon>
+          </template>
+        </v-text-field>
+
+        <div class="font-weight-medium mb-2">Confirmar senha</div>
+        <v-text-field
+          v-model="confirmPassword"
+          placeholder="Confirmar senha"
+          :type="confirmPasswordVisible ? 'text' : 'password'"
+          required
+        >
+          <template #append-inner>
+            <v-icon
+              v-if="confirmPasswordVisible"
+              class="cursor-pointer"
+              @click="toggleConfirmPasswordVisibility"
+            >
+              mdi-eye-off
+            </v-icon>
+            <v-icon
+              v-else
+              class="cursor-pointer"
+              @click="toggleConfirmPasswordVisibility"
+            >
+              mdi-eye
+            </v-icon>
+          </template>
+        </v-text-field>
       </v-card-text>
+
+      <v-card-actions class="px-4">
+        <v-spacer />
+        <v-btn
+          class="btn btn-primary px-6 ml-2"
+          :class="{
+            'btn-disabled':
+              !currentPassword || !newPassword || !confirmPassword,
+          }"
+          @click="changePassword"
+        >
+          Salvar
+        </v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-
 const userStore = useUserStore();
+const snackBarStore = useSnackbarStore();
 const showDialog = ref(false);
-const currentPassword = ref("");
-const newPassword = ref("");
-const confirmPassword = ref("");
+const currentPassword = ref();
+const newPassword = ref();
+const confirmPassword = ref();
+
+const currentPasswordVisible = ref(false);
+const newPasswordVisible = ref(false);
+const confirmPasswordVisible = ref(false);
+
+function toggleCurrentPasswordVisibility() {
+  currentPasswordVisible.value = !currentPasswordVisible.value;
+}
+
+function toggleNewPasswordVisibility() {
+  newPasswordVisible.value = !newPasswordVisible.value;
+}
+
+function toggleConfirmPasswordVisibility() {
+  confirmPasswordVisible.value = !confirmPasswordVisible.value;
+}
 
 function changePassword() {
   if (newPassword.value === confirmPassword.value) {
     if (userStore.currentUser?.id !== undefined) {
       userStore.updateUserPassword(newPassword.value, userStore.currentUser.id);
-    } else {
-      alert("User ID is not available");
+      snackBarStore.showSnackbar("success", "Senha alterada com sucesso!");
     }
     showDialog.value = false;
+    reset();
   } else {
-    alert("As senhas não coincidem");
+    snackBarStore.showSnackbar("error", "As senhas não coincidem");
   }
+}
+
+function cancel() {
+  showDialog.value = false;
+  reset();
+}
+
+function reset() {
+  currentPassword.value = null;
+  newPassword.value = null;
+  confirmPassword.value = null;
 }
 
 defineExpose({
   showDialog,
 });
 </script>
+
+<style scoped>
+:deep(.v-input__prepend > .v-icon),
+:deep(.v-field__append-inner > .v-icon, ) {
+  opacity: 1;
+  color: rgba(var(--v-theme-primary));
+}
+</style>
